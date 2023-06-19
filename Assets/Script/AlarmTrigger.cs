388c1549ab9003886d3df8558a4e4b366fa19008
XOF0XC0000005 <leva95reva@yahoo.com>
2023-06-19 10:09:01 +0300

    Fixing v2

--- Assets/Script/AlarmTrigger.cs
+++ Assets/Script/AlarmTrigger.cs
@@ -7,63 +7,61 @@ using UnityEngine.Events;
 public class AlarmTrigger : MonoBehaviour
 {
     [SerializeField] private UnityEvent _triggered;
-    [SerializeField] private float _volumeSpeed = 0.0001f;
+    [SerializeField] private float _volumeSpeed = 0.3f;
 
     private AudioSource _audioSource;
     private float _minVolume = 0f;
     private float _maxVolume = 1f;
-    private Coroutine lastIncreasedRoutine = null;
-    private Coroutine lastDecreasedRoutine = null;
-
-    public void IncreaseVolumeToMax()
-    {
-        _triggered?.Invoke();
-
-        if (lastDecreasedRoutine != null)
-        {
-            StopCoroutine(lastDecreasedRoutine);
-            lastDecreasedRoutine = null;
-        }
-
-        if (lastIncreasedRoutine == null)
-        {
-            lastIncreasedRoutine = StartCoroutine(ManipulateVolume(_audioSource.volume, _maxVolume, _volumeSpeed));
-        }
-    }
-
-    public void DeacreaseVolumeToMin()
-    {
-        if (lastIncreasedRoutine != null)
-        {
-            StopCoroutine(lastIncreasedRoutine);
-            lastIncreasedRoutine = null;
-        }
-
-        if (lastDecreasedRoutine == null)
-        {
-            lastDecreasedRoutine = StartCoroutine(ManipulateVolume(_audioSource.volume, _minVolume, _volumeSpeed));
-        }
-    }
-
-    private IEnumerator ManipulateVolume(float currentVolume, float targetVolume, float volumeSpeed)
-    {
-        while(_audioSource.volume != targetVolume)
-        {
-            _audioSource.volume = Mathf.MoveTowards(currentVolume, targetVolume, volumeSpeed);
-            volumeSpeed += volumeSpeed * Time.deltaTime;
-            
-            if (_audioSource.volume == _minVolume) 
-            {
-                _audioSource.Stop();
-            }
-            
-            yield return null;
-        }
-    }
+    private Coroutine _lastRoutine = null;
 
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
         _audioSource.volume = 0f;
+    }
+
+    private IEnumerator ManipulateVolume(float targetVolume, float volumeSpeed)
+    {
+        while (_audioSource.volume != targetVolume)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, targetVolume, volumeSpeed);
+
+            if (_audioSource.volume == _minVolume)
+            {
+                _audioSource.Stop();
+            }
+
+            yield return null;
+        }
+    }
+
+    public void IncreaseVolumeToMax()
+    {
+        _triggered?.Invoke();
+
+        if (_lastRoutine != null)
+        {
+            StopCoroutine(_lastRoutine);
+            _lastRoutine = null;
+            _lastRoutine = StartCoroutine(ManipulateVolume(_maxVolume, _volumeSpeed));
+        }
+        else
+        {
+            _lastRoutine = StartCoroutine(ManipulateVolume(_maxVolume, _volumeSpeed));
+        }
+    }
+
+    public void DeacreaseVolumeToMin()
+    {
+        if (_lastRoutine != null)
+        {
+            StopCoroutine(_lastRoutine);
+            _lastRoutine = null;
+            _lastRoutine = StartCoroutine(ManipulateVolume(_minVolume, _volumeSpeed));
+        }
+        else
+        {
+            _lastRoutine = StartCoroutine(ManipulateVolume(_minVolume, _volumeSpeed));
+        }
     }
 }
